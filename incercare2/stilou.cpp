f041dfe162d19a8afa6f2b44f51683322cda9aae
GasparGeorgiana <73753872+GasparGeorgiana@users.noreply.github.com>
2021-01-19 21:31:31 +0200

    Update stilou.cpp

--- incercare2/stilou.cpp
+++ incercare2/stilou.cpp
@@ -1,7 +1,7 @@
-#include <cstring>
+#include <xstring>
 #include "produs.h"
 #include "stilou.h"
-
+ 
 stilou::stilou(const std::string &denumire, const std::string &producator, float pret, int nrProduse, const std::string &tipRezerva,
                const std::string &tipPenita, const std::string &material):
                produs(denumire, producator, pret, nrProduse)
@@ -10,14 +10,15 @@ stilou::stilou(const std::string &denumire, const std::string &producator, float
                     this->tip_penita = tipPenita;
                     this->material = material;
                }
-
+ 
 std::ostream &operator<<(std::ostream &os, const stilou &stilou) {
     os << stilou.getDenumire() << '\n' << "Producator: " << stilou.getProducator() << '\n' << "Pret: " << stilou.getPret() << '\n' << "Nr produse: " << stilou.getNr_produse()<< '\n'
        << "Tip rezerva: " << stilou.tip_rezerva << '\n' << "Tip penita: " << stilou.tip_penita << '\n' << "Material: " << stilou.material << '\n';
     return os;
 }
-
+ 
 void stilou::verificare_produs()
 {
-
+    if(getPret() >= 50)
+        throw "Ati primit o reducere de 20%";
 }
