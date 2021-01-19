b308f2e691f13e65cd7aa90cc9f2537e6c0d282a
GasparGeorgiana <73753872+GasparGeorgiana@users.noreply.github.com>
2021-01-19 21:32:14 +0200

    Update penar.h

--- incercare2/penar.h
+++ incercare2/penar.h
@@ -1,12 +1,10 @@
-#ifndef PENAR_H
-#define PENAR_H
 #include <iostream>
 #include <cstring>
 #include "produs.h"
 #include "creion_mecanic.h"
 #include "stilou.h"
-#include "set_mine.h"
-
+#include "mine.h"
+ 
 class penar: public produs
 {
     std::string forma;
@@ -14,15 +12,15 @@ class penar: public produs
     std::string dimensiune;
     creion_mecanic cm;
     stilou st;
-
+    set_mine sm;
+ 
 public:
     penar(const std::string &denumire, const std::string &producator, float pret, int nrProduse,
           const std::string &forma, int nrIncaperi, const std::string &dimensiune, const creion_mecanic &cm,
           const stilou &st, const set_mine &sm);
-
+ 
     friend std::ostream &operator<<(std::ostream &os, const penar &penar);
-
+ 
     void verificare_produs() override;
 };
-
 #endif // PENAR_H
