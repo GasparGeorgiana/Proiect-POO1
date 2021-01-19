a881679da4d6c8ba4ceaa3cb809c3638d07af312
GasparGeorgiana <73753872+GasparGeorgiana@users.noreply.github.com>
2021-01-19 21:33:22 +0200

    Update penar.cpp

--- incercare2/penar.cpp
+++ incercare2/penar.cpp
@@ -1,25 +1,26 @@
-#include <cstring>
+#include <xstring>
 #include "creion_mecanic.h"
 #include "stilou.h"
 #include "penar.h"
-#include "set_mine.h"
-
+ 
 penar::penar(const std::string &denumire, const std::string &producator, float pret, int nrProduse, const std::string &forma,
              int nrIncaperi, const std::string &dimensiune, const creion_mecanic &cm, const stilou &st, const set_mine &sm):
-             produs(denumire, producator, pret, nrProduse), cm(cm), st(st)
+             produs(denumire, producator, pret, nrProduse), cm(cm), st(st), sm(sm)
              {
                     this->forma = forma;
                     this->nr_incaperi = nrIncaperi;
                     this->dimensiune = dimensiune;
              }
-
+ 
 std::ostream &operator<<(std::ostream &os, const penar &penar) {
     os << penar.getDenumire() << '\n' << "Producator: " << penar.getProducator() << '\n' << "Pret: " << penar.getPret() << '\n' << "Nr produse: " << penar.getNr_produse()<< '\n'
-       << "Forma: " << penar.forma << '\n' << "Nr incaperi: " << penar.nr_incaperi << '\n' << "Dimensiune: " << penar.dimensiune << '\n' << "Contine:\n" << "Creion: " << penar.cm << '\n' << "Stilou: " << penar.st;
+       << "Forma: " << penar.forma << '\n' << "Nr incaperi: " << penar.nr_incaperi << '\n' << "Dimensiune: " << penar.dimensiune << '\n' << "Contine:\n" << "Creion: "
+       << penar.cm << '\n' << "Stilou: " << penar.st << '\n' << "Mine: " << penar.sm;
     return os;
 }
-
+ 
 void penar::verificare_produs()
 {
-
+    if(getProducator() == cm.getProducator())
+        throw "Primiti un creion gratis";
 }
